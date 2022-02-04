b44d465c1dba91e8289bf879cf1b3baaf2cb3531
pavelhardzei <pavelgordei11@gmail.com>
2022-02-04 15:15:00 +0300

    added signin endpoint

--- app/users/schemas.py
+++ app/users/schemas.py
@@ -1,5 +1,7 @@
+from marshmallow import ValidationError, post_load
 from project import ma
 from users.models import UserProfile
+from werkzeug.security import check_password_hash, generate_password_hash
 
 
 class UserProfileSchema(ma.SQLAlchemyAutoSchema):
@@ -8,3 +10,24 @@ class UserProfileSchema(ma.SQLAlchemyAutoSchema):
         fields = ('id', 'email', 'username', 'password')
         load_only = ('password', )
         load_instance = True
+
+
+class UserSigninSchema(ma.Schema):
+    email = ma.Email()
+    password = ma.String()
+
+    @post_load
+    def check_password(self, data, *args, **kwargs):
+        user = UserProfile.query.filter_by(email=data['email']).first()
+        if user is None:
+            raise ValidationError({'message': 'User not found'})
+
+        if not check_password_hash(user.password, data['password']):
+            raise ValidationError({'message': 'Password is incorrect'})
+
+        return user
+
+
+class UserTokenSchema(ma.Schema):
+    user = ma.Nested(UserProfileSchema)
+    token = ma.String()
--- app/users/views.py
+++ app/users/views.py
@@ -1,9 +1,13 @@
+from datetime import datetime, timedelta
+
+import jwt
 from flask import Blueprint, request
 from flask_restful import Api, Resource
+from project import app as root
 from project import db
 from users.models import UserProfile
-from users.schemas import UserProfileSchema
-from werkzeug.security import check_password_hash, generate_password_hash
+from users.schemas import UserProfileSchema, UserSigninSchema, UserTokenSchema
+from werkzeug.security import generate_password_hash
 
 app = Blueprint('users', __name__)
 api = Api(app)
@@ -22,4 +26,16 @@ class UserSignup(Resource):
         return self.schema.dump(user)
 
 
+class UserSignin(Resource):
+    schema = UserSigninSchema()
+
+    def post(self):
+        user = self.schema.load(request.get_json())
+        token = jwt.encode({'id': user.id, 'exp': datetime.utcnow() + timedelta(seconds=10)},
+                           root.config['SECRET_KEY'], algorithm='HS256')
+
+        return UserTokenSchema().dump({'user': user, 'token': token})
+
+
 api.add_resource(UserSignup, '/signup/')
+api.add_resource(UserSignin, '/signin/')
