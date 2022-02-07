10ed37ebcac939ce0e237f41086d7b20ef790c71
pavelhardzei <pavelgordei11@gmail.com>
2022-02-07 15:23:37 +0300

    added delete property

--- app/applications/models.py
+++ app/applications/models.py
@@ -11,5 +11,9 @@ class Application(db.Model):
         db.session.add(self)
         db.session.commit()
 
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
     def __repr__(self):
         return f'Application({self.name})'
