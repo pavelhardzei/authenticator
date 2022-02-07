f7610bf2c1d7bcff305f9e43d205e1752cc32b0c
pavelhardzei <pavelgordei11@gmail.com>
2022-02-07 11:58:15 +0300

    added applications model

---
+++ app/applications/models.py
@@ -0,0 +1,16 @@
+from project import db
+
+
+class Application(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    secret = db.Column(db.String(50), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user_profile.id'), nullable=False)
+    user = db.relationship('UserProfile', lazy=True, backref=db.backref('applications', lazy=True))
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return f'Application({self.name})'
