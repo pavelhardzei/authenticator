1a315a68262794219f7425d644cf80d8b1c19f29
pavelhardzei <pavelgordei11@gmail.com>
2022-02-07 16:28:17 +0300

    added app detail endpoint

--- app/applications/views.py
+++ app/applications/views.py
@@ -2,6 +2,7 @@ from applications.models import Application
 from applications.schemas import ApplicationSchema
 from flask import Blueprint, request
 from flask_restful import Api, Resource
+from project.exceptions import LogicError
 from project.utils import token_required
 
 app = Blueprint('applications', __name__)
@@ -26,7 +27,47 @@ class ApplicationList(Resource):
         application.user_id = user.id
         application.save()
 
+        return self.schema.dump(application), 201
+
+
+class ApplicationDetail(Resource):
+    schema = ApplicationSchema()
+
+    @token_required
+    def get(self, *args, **kwargs):
+        return self.schema.dump(self.get_object(*args, **kwargs))
+
+    @token_required
+    def delete(self, *args, **kwargs):
+        application = self.get_object(*args, **kwargs)
+        application.delete()
+
+        return {'message': 'No content'}, 204
+
+    @token_required
+    def put(self, *args, **kwargs):
+        schema = ApplicationSchema(exclude=('secret', ))
+
+        instance = self.get_object(*args, **kwargs)
+        application = schema.load(request.get_json(), instance=instance)
+        application.save()
+
         return self.schema.dump(application)
+
+    def get_object(self, *args, **kwargs):
+        user = kwargs['user']
+        application = Application.query.filter_by(id=kwargs['id']).first()
+
+        self.check_permissions(user, application)
+
+        return application
+
+    def check_permissions(self, user, obj):
+        if obj is None:
+            raise LogicError(404, 'Resource not found')
+        if obj.user_id != user.id:
+            raise LogicError(403, 'Forbidden')
 
 
 api.add_resource(ApplicationList, '/')
+api.add_resource(ApplicationDetail, '/<int:id>/')
