788b0a55a65b359b374878fded689f664bd959b9
pavelhardzei <pavelgordei11@gmail.com>
2022-02-11 15:36:50 +0300

    removed unnecessary

--- app/users/conftest.py
+++ app/users/conftest.py
@@ -1,6 +1,5 @@
-from project.conftest import api_client, connection, session, token_user1, token_user2
+from project.conftest import api_client, connection, session, token_user1
 from pytest_factoryboy import register
 from users.factories import UserFactory
 
 register(UserFactory, 'user1', email='user1@gmail.com', username='user1')
-register(UserFactory, 'user2')
