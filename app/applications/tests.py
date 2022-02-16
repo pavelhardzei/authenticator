91e591382eec127eda0345f4855fc237c0e81950
pavelhardzei <pavelgordei11@gmail.com>
2022-02-16 11:57:45 +0300

    replaced codes

--- app/applications/tests.py
+++ app/applications/tests.py
@@ -1,3 +1,4 @@
+from http import HTTPStatus
 from unittest.mock import ANY
 
 import pyotp
@@ -5,43 +6,43 @@ import pyotp
 
 def test_application_list(api_client, token_user1, app1_user1, app2_user1):
     response = api_client.get('/application/', headers={'Authorization': f'Token {token_user1}'})
-    assert response.status_code == 200
+    assert response.status_code == HTTPStatus.OK
     assert response.json == [{'id': ANY, 'name': 'app1'}, {'id': ANY, 'name': 'app2'}]
 
 
 def test_application_post(api_client, token_user1):
     response = api_client.post('/application/', headers={'Authorization': f'Token {token_user1}'},
                                json={'name': 'app', 'secret': 'base32secret'})
-    assert response.status_code == 201
+    assert response.status_code == HTTPStatus.CREATED
     assert response.json == {'id': ANY, 'name': 'app'}
 
 
 def test_application_detail(api_client, token_user1, app1_user1):
     response = api_client.get(f'/application/{app1_user1.id}/', headers={'Authorization': f'Token {token_user1}'})
-    assert response.status_code == 200
+    assert response.status_code == HTTPStatus.OK
     assert response.json == {'id': app1_user1.id, 'name': 'app1'}
 
 
 def test_application_detail_violation(api_client, token_user1, app1_user2):
     response = api_client.get(f'/application/{app1_user2.id}/', headers={'Authorization': f'Token {token_user1}'})
-    assert response.status_code == 404
+    assert response.status_code == HTTPStatus.NOT_FOUND
     assert response.json == {'message': 'Resource not found'}
 
 
 def test_application_put(api_client, token_user1, app1_user1):
     response = api_client.put(f'/application/{app1_user1.id}/', headers={'Authorization': f'Token {token_user1}'},
                               json={'name': 'new'})
-    assert response.status_code == 200
+    assert response.status_code == HTTPStatus.OK
     assert response.json == {'id': app1_user1.id, 'name': 'new'}
 
 
 def test_application_delete(api_client, token_user1, app1_user1):
     response = api_client.delete(f'/application/{app1_user1.id}/', headers={'Authorization': f'Token {token_user1}'})
-    assert response.status_code == 204
+    assert response.status_code == HTTPStatus.NO_CONTENT
 
 
 def test_application_code(api_client, token_user2, app1_user2):
     response = api_client.get(f'/application/{app1_user2.id}/code/',
                               headers={'Authorization': f'Token {token_user2}'})
-    assert response.status_code == 200
+    assert response.status_code == HTTPStatus.OK
     assert response.json == {'totp': pyotp.TOTP('base32secret').now()}
--- app/users/tests.py
+++ app/users/tests.py
@@ -1,45 +1,52 @@
+from http import HTTPStatus
 from unittest.mock import ANY
 
 
 def test_signup(api_client):
     response = api_client.post('/user/signup/', json={'email': 'test@test.org', 'username': 'test',
                                                       'password': 'testing321'})
-    assert response.status_code == 201
+    assert response.status_code == HTTPStatus.CREATED
     assert response.json == {'id': ANY, 'email': 'test@test.org', 'username': 'test'}
 
 
 def test_signin(api_client, user1):
     response = api_client.post('/user/signin/', json={'email': 'user1@gmail.com', 'password': 'testing321'})
-    assert response.status_code == 200
+    assert response.status_code == HTTPStatus.OK
     assert response.json == {'user': {'id': ANY, 'email': 'user1@gmail.com', 'username': 'user1'}, 'token': ANY}
 
 
 def test_user_detail(api_client, token_user1):
     response = api_client.get('/user/', headers={'Authorization': f'Token {token_user1}'})
-    assert response.status_code == 200
+    assert response.status_code == HTTPStatus.OK
     assert response.json == {'id': ANY, 'email': 'user1@gmail.com', 'username': 'user1'}
+
+
+def test_user_detail_unauthorized(api_client):
+    response = api_client.get('/user/')
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+    assert response.json == {'message': 'Token is required'}
 
 
 def test_user_put(api_client, token_user1):
     response = api_client.put('/user/', headers={'Authorization': f'Token {token_user1}'},
                               json={'email': 'new@gmail.com', 'username': 'new'})
-    assert response.status_code == 200
+    assert response.status_code == HTTPStatus.OK
     assert response.json == {'id': ANY, 'email': 'new@gmail.com', 'username': 'new'}
 
 
 def test_user_invalid_put(api_client, token_user1):
     response = api_client.put('/user/', headers={'Authorization': f'Token {token_user1}'},
                               json={'email': 'new@gmail.com', 'username': 'new', 'password': 'testing321'})
-    assert response.status_code == 400
+    assert response.status_code == HTTPStatus.BAD_REQUEST
     assert response.json == {'message': [{'password': ['Unknown field.']}]}
 
 
 def test_user_patch(api_client, token_user1):
     response = api_client.patch('/user/', headers={'Authorization': f'Token {token_user1}'}, json={'username': 'new'})
-    assert response.status_code == 200
+    assert response.status_code == HTTPStatus.OK
     assert response.json == {'id': ANY, 'email': 'user1@gmail.com', 'username': 'new'}
 
 
 def test_user_delete(api_client, token_user1):
     response = api_client.delete('/user/', headers={'Authorization': f'Token {token_user1}'})
-    assert response.status_code == 204
+    assert response.status_code == HTTPStatus.NO_CONTENT
